2586935a1e07d5b511f3246423ab0be7c4154579
fatmayilmaz209 <f4atmayilmaz209@hotmail.com>
2022-02-12 15:52:19 +0300

    fixed

--- kitap_pazari/kitap_pazari/settings.py
+++ kitap_pazari/kitap_pazari/settings.py
@@ -123,8 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+
 
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Default primary key field type
